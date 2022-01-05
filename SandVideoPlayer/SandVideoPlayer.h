3e27269625f7c2099a5f695027e777807a18ad5c
kemvanra ouch <ouchkemvanra1@gmail.com>
2022-01-05 10:15:22 +0700

    Initial Commit

---
+++ SandVideoPlayer/SandVideoPlayer.h
@@ -0,0 +1,18 @@
+//
+//  SandVideoPlayer.h
+//  SandVideoPlayer
+//
+//  Created by Ouch Kemvanra on 1/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SandVideoPlayer.
+FOUNDATION_EXPORT double SandVideoPlayerVersionNumber;
+
+//! Project version string for SandVideoPlayer.
+FOUNDATION_EXPORT const unsigned char SandVideoPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SandVideoPlayer/PublicHeader.h>
+
+
